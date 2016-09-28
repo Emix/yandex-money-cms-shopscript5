66c2525e33a25fc4fa3668d37dbc4ac6ec369bc4
ikarmanov <ivkarmanov@yamoney.ru>
2016-09-28 16:44:48 +0300

    Войдет в 1.2.4 Исправлена ссылка на регистрацию приложения в кошельке Яндекс.Денег

--- src/wa-plugins/payment/yamodulepay/lib/config/plugin.php
+++ src/wa-plugins/payment/yamodulepay/lib/config/plugin.php
@@ -5,5 +5,5 @@ return array(
     'icon'        => 'img/yandexmoney16.png',
     'logo'        => 'img/yandexmoney.png',
     'vendor'      => 'webasyst',
-    'version'     => '1.2.3.4',
+    'version'     => '1.2.4',
 );
