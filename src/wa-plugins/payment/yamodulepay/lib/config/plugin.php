44b80c3a08b002baf2c7056106932eec5a0fb914
ikarmanov <ivkarmanov@yamoney.ru>
2016-09-29 14:59:37 +0300

    Войдет в 1.2.4 Обновлен логотип Яндекс.Кассы

--- src/wa-plugins/payment/yamodulepay/lib/config/plugin.php
+++ src/wa-plugins/payment/yamodulepay/lib/config/plugin.php
@@ -3,7 +3,7 @@ return array(
     'name'        => 'Яндекс.Деньги (Y.CMS)',
     'description' => 'Платежный сервис «<a href="http://money.yandex.ru">Яндекс.Деньги</a>»',
     'icon'        => 'img/yandexmoney16.png',
-    'logo'        => 'img/yandexmoney.png',
+    'logo'        => 'img/kassa-logo.png',
     'vendor'      => 'webasyst',
     'version'     => '1.2.4',
 );
